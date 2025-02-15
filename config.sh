d64c238fbd35e5ab0475249c18c8d11bb8d7f4bd
nathan lempereur <167067383+nathanlempereur@users.noreply.github.com>
2025-02-15 11:49:24 +0100

    Add files via upload

---
+++ config.sh
@@ -0,0 +1,9 @@
+
+#!/bin/bash
+
+#permet de determiner si programme sur un serveur (1) ou une machine (0). Utile pour IHM
+serveur=0
+#permet de determiner si le mode d'altonien est actif ou non.
+couleur=0
+#permet d'afficher le nom de l'utilisateur
+nom=Nathan
